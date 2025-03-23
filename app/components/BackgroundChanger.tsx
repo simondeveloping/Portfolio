9e748f94b1e108de76dcbbec3a7b082c27c00012
yv521464 <simon.pham2005@gmail.com>
2025-03-23 12:44:46 +0100

    make background changer for tablet

--- app/components/BackgroundChanger.tsx
+++ app/components/BackgroundChanger.tsx
@@ -5,10 +5,12 @@ import { useEffect, useState } from "react";
 export default function BackgroundChanger() {
   const [bgClass, setBgClass] = useState("bg-cream");
   const [isMobile, setIsMobile] = useState(false);
+  const [isTablet, setIsTablet] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768); // Bildschirmgröße für Handys, z.B. 768px
+      setIsTablet(window.innerWidth <= 1200 && window.innerWidth > 768); // Bildschirmgröße für Tablets, z.B. 1024px
     };
 
     const handleScroll = () => {
@@ -19,6 +21,18 @@ export default function BackgroundChanger() {
         if (scrollY > 100 && scrollY < 500) {
           setBgClass("bg-cookie");
         } else if (scrollY >= 500 && scrollY < 3000) {
+          setBgClass("bg-cookie");
+        } else if (scrollY >= 3000 && scrollY < 5000) {
+          setBgClass("bg-cream");
+        } else if (scrollY >= 5000) {
+          setBgClass("bg-blay");
+        } else {
+          setBgClass("bg-cream");
+        }
+      } else if (isTablet) {
+        if (scrollY > 200 && scrollY < 600) {
+          setBgClass("bg-cookie");
+        } else if (scrollY >= 600 && scrollY < 3000) {
           setBgClass("bg-cookie");
         } else if (scrollY >= 3000 && scrollY < 5000) {
           setBgClass("bg-cream");
@@ -44,7 +58,7 @@ export default function BackgroundChanger() {
         }
       }
     };
-
+    console.log(isTablet);
     window.addEventListener("resize", handleResize);
     window.addEventListener("scroll", handleScroll);
 
@@ -55,7 +69,7 @@ export default function BackgroundChanger() {
       window.removeEventListener("resize", handleResize);
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [isMobile]);
+  }, [isMobile, isTablet]);
 
   return (
     <div
